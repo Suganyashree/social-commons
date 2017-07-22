6f8ccfc870056df7876c58439879569a06b03c6b
Suganyashree <suganya.gc@gmail.com>
2017-07-22 11:25:28 +0530

    first commit

---
+++ src/main/java/com/athena/social/automation/scripts/webapp/page_objects/dashboard/DashBoard_Page.java
@@ -0,0 +1,11 @@
+package com.athena.social.automation.scripts.webapp.page_objects.dashboard;
+
+import org.openqa.selenium.WebDriver;
+
+public class DashBoard_Page {
+	public DashBoard_Page_Objects webElements;
+
+	public DashBoard_Page(WebDriver driver) {
+		this.webElements = new DashBoard_Page_Objects(driver);
+	}
+}
---
+++ src/main/java/com/athena/social/automation/scripts/webapp/page_objects/dashboard/DashBoard_Page_Objects.java
@@ -0,0 +1,26 @@
+package com.athena.social.automation.scripts.webapp.page_objects.dashboard;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class DashBoard_Page_Objects {
+	private WebDriver driver;
+
+	public DashBoard_Page_Objects(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	public WebElement top_destination(){
+		return driver.findElement(By.linkText("Top Destinations"));
+	}
+	public WebElement top_destination_more(){
+		return driver.findElement(By.linkText("More..."));
+	}
+	public WebElement commons_home_page(){
+		return driver.findElement(By.id("logo"));
+	}
+	public WebElement ask_hub(){
+		return driver.findElement(By.linkText("askHub"));
+	}
+}
